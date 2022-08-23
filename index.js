73333ae24e1b5a6d521ef191e5ea7a5851e44d67
flyingjjang051 <flyjjang051@gmail.com>
2022-08-23 11:33:53 +0900

    cloudinary에 파일 업로드

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const app = express();
 const dotenv = require("dotenv").config();
 const path = require("path");
 const multer = require("multer");
+const cloudinary = require("cloudinary");
+
+const MongoClient = require("mongodb").MongoClient;
+let db = null;
+MongoClient.connect(process.env.MONGO_URL, { useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.log(err);
+  }
+  db = client.db("crudapp");
+});
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false })); // post에서 보낸 데이터 req.body로 받을려면 있어야함
@@ -11,12 +21,18 @@ app.use("/upload", express.static(path.join(__dirname, "/upload")));
 app.set("port", process.env.PORT || 8099);
 const PORT = app.get("port");
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_COUND_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, path.join(__dirname, "/upload"));
+  destination: (req, file, done) => {
+    done(null, path.join(__dirname, "/upload"));
   },
-  filename: (req, file, callback) => {
-    callback(null, file.originalname);
+  filename: (req, file, done) => {
+    done(null, file.originalname);
   },
 });
 
@@ -36,14 +52,25 @@ app.post("/register", fileUpload.single("poster"), (req, res) => {
   const desc = req.body.desc;
   const point = req.body.point;
 
-  console.log(movieTitle);
-  console.log(date);
-  console.log(genre);
-  console.log(desc);
-  console.log(point);
-  console.log(req.file);
+  // console.log(movieTitle);
+  // console.log(date);
+  // console.log(genre);
+  // console.log(desc);
+  // console.log(point);
+  // console.log(req.file);
   // db에 파일을 저장하는 두가지 방법....  text로 바꿔서 저장
   // db에다가 경로만 저장 (upload/01.jpg)
+  cloudinary.uploader.upload(req.file.path, (result) => {
+    console.log(result);
+    db.collection("movie").insertOne({
+      movieTitle: movieTitle,
+      date: date,
+      genre: genre,
+      desc: desc,
+      point: point,
+      poster: result.url,
+    });
+  });
   res.send("파일 전송 완료");
 });
 
